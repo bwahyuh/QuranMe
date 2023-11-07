1b0caaf67e6a9007daa3875c134aa063806f60de
--global <21523283@students.uii.ac.id>
2023-11-08 04:05:45 +0700

    Adding poppins font and Get Started Page
    50% done

--- app/src/main/java/com/example/quranme/MainActivity.kt
+++ app/src/main/java/com/example/quranme/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.quranme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.quranme.ui.theme.QuranMeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +35,130 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Background()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Title("QuranMe")
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Description("Learn Quran and Recite once everyday")
+
+                        Spacer(modifier = Modifier.height(8.dp))
+                        ButtonRect(
+                            onClick = { /*TODO*/ }, "Get Started")
+                        }
+                    }
+                    Column {
+
+                    }
                 }
             }
         }
     }
+
+
+@Composable
+fun Background() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+
+//        shape = RoundedCornerShape(size = 40.dp),
+        color = Color(0xFF040C23)
+    ) {
+        // Background does not need to contain other composables
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Title(name: String, modifier: Modifier = Modifier) {
+    val poppinsFamily = FontFamily(
+        Font(R.font.poppins_bold, FontWeight.Normal)
+    )
+
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        text = name,
+        style = TextStyle(
+            fontSize = 28.sp,
+            fontFamily = poppinsFamily,
+            fontWeight = FontWeight.W700,
+            color = Color.White, // Changed to white for visibility on the dark background
+            textAlign = TextAlign.Center
+        )
     )
 }
 
+@Composable
+fun Description(desc: String, modifier: Modifier = Modifier) {
+    val poppinsFamily = FontFamily(
+        Font(R.font.poppins_light, FontWeight.Normal)
+    )
+
+    Text(
+        modifier = modifier,
+        text = desc,
+        style = TextStyle(
+            fontSize = 18.sp,
+            fontFamily = poppinsFamily,
+            fontWeight = FontWeight.W400, // Use FontWeight object
+            color = Color(0xFFA19CC5),
+            textAlign = TextAlign.Center,
+        )
+    )
+}
+@Composable
+fun ButtonRect(onClick: () -> Unit, name: String, modifier: Modifier = Modifier){
+    val poppinsFamily = FontFamily(
+        Font(R.font.poppins_regular, FontWeight.Normal)
+    )
+
+    Button(
+        onClick = onClick,
+        shape = RoundedCornerShape(30.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF9B091)),
+        modifier = Modifier
+            .width(185.dp)
+            .height(60.dp)
+    ) {
+
+        Text(
+            text = name,
+            style = TextStyle(
+                fontSize = 18.sp,
+                fontFamily = FontFamily(Font(R.font.poppins_bold)),
+                fontWeight = FontWeight(600),
+                color = Color(0xFF091945),
+                textAlign = TextAlign.Center,
+            )
+        )
+    }
+
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     QuranMeTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Background()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Title("QuranMe")
+                Spacer(modifier = Modifier.height(8.dp))
+                Description("Learn Quran and Recite once everyday")
+            }
+        }
     }
 }
